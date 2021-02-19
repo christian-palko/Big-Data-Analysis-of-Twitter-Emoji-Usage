59bf724789b84c66ac4e8032004e174e75b4f805
Conor Sosh <conorsosh@gmail.com>
2021-02-19 12:11:20 -0500

    Completed Question One
    Improved emoji counting routine, updated regex, changed method names, changed column names during dataframe transformations

--- q1/src/main/scala/com/revature/questionone/Runner.scala
+++ q1/src/main/scala/com/revature/questionone/Runner.scala
@@ -12,8 +12,14 @@ import java.io.PrintWriter
 import java.nio.file.Paths
 import java.nio.file.Files
 import scala.concurrent.Future
-import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions._
 
+/**
+  * Runner program to answer Question One of our project:
+  *   What are the most used emoji currently?
+  * Takes from Twitter's sampled stream to retrieve tweets in real time,
+  *   and parses them down to just emojis, and gets a count per emoji.
+  */
 object Runner {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
@@ -25,19 +31,21 @@ object Runner {
     import spark.implicits._
     spark.sparkContext.setLogLevel("WARN")
 
-    tweetStream(spark)
+    parseTweetStream(spark)
   }
 
-  def tweetStream(spark: SparkSession): Unit = {
+  def parseTweetStream(spark: SparkSession): Unit = {
     import spark.implicits._
 
     val bearerToken = System.getenv("TWITTER_BEARER_TOKEN")
 
     import scala.concurrent.ExecutionContext.Implicits.global
     Future {
-      emojiTweetTest(bearerToken)
+      downloadTweetStream(bearerToken)
     }
 
+    // Routine to wait for a file to appear in the twitterstream folder.
+    // Note: Ends the program if no file appears after 30 seconds.
     var start = System.currentTimeMillis()
     var filesFoundInDir = false
     while (!filesFoundInDir && (System.currentTimeMillis() - start) < 30000) {
@@ -56,28 +64,32 @@ object Runner {
     val streamDf =
       spark.readStream.schema(staticDf.schema).json("twitterstream")
 
-    val pattern = ".*([^\u0000-\uFFFF]).*".r
+    val emoji = "[(\uD83D\uDE00-\uD83D\uDE4F)|(\uD83C\uDF00-\uD83D\uDDFF)|(\uD83E\uDD00-\uD83E\uDDFF)]"
+    val notEmoji = "[^(\uD83D\uDE00-\uD83D\uDE4F)|(\uD83C\uDF00-\uD83D\uDDFF)|(\uD83E\uDD00-\uD83E\uDDFF)]"
+    val regexSpace = "(\\B\uD83D.{1})|(\\B\uD83C.{1})|(\\B\uD83E.{1})"
+
     streamDf
       .select($"data.text")
-      .as[String]
-      .flatMap(text => {
-        text match {
-          case pattern(emote) => { Some(emote) }
-          case notFound        => None
-        }
-      })
-      .groupBy("value")
-      .count()
-      .sort(functions.desc("count"))
+      .filter($"text" rlike s"$emoji")
+      .select(regexp_replace($"text", s"$notEmoji", "").as("Removed Text"))
+      .select(regexp_replace($"Removed Text", s"$regexSpace", " $1 ").as("Added Emoji Space"))
+      .select(trim($"Added Emoji Space").as("Trimmed"))
+      .select(split($"Trimmed", " ").as("Split"))
+      .select(explode($"Split").as("Emoji"))
+      .filter($"Emoji" rlike s"$emoji")
+      .filter(!$"Emoji".contains("(") && !$"Emoji".contains(")") && !$"Emoji".contains("|"))
+      .groupBy($"Emoji")
+      .count().as("Count")
+      .orderBy(desc("Count"), $"Emoji")
       .writeStream
       .outputMode("complete")
       .format("console")
+      .option("truncate", false)
       .start()
       .awaitTermination()
-
   }
 
-  def emojiTweetTest(
+  def downloadTweetStream(
       bearerToken: String,
       dirname: String = "twitterstream",
       linesPerFile: Int = 1000,
