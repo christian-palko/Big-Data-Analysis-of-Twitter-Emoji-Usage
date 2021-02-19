c8a76a2eb990a6d380b29a739476c1952f1588f8
Conor Sosh <conorsosh@gmail.com>
2021-02-19 16:45:02 -0500

    Added new method
    Added a method to specify a single user to see how they are tagged with emojis.

--- q4/src/main/scala/com/revature/questionfour/Runner.scala
+++ q4/src/main/scala/com/revature/questionfour/Runner.scala
@@ -13,6 +13,7 @@ import java.nio.file.Paths
 import java.nio.file.Files
 import scala.concurrent.Future
 import org.apache.spark.sql.functions._
+import scala.io.StdIn
 
 /**
   * Runner program to answer Question Four of our project:
@@ -30,12 +31,105 @@ object Runner {
       .getOrCreate()
 
     import spark.implicits._
+
     spark.sparkContext.setLogLevel("WARN")
 
-    parseTweetStream(spark)
+    var continue = true
+    while (continue){
+      println("Options:\n" +
+        "1 - Get emoji count for ALL users\n" +
+        "2 - Get emoji count for a specific user\n" +
+        "3 - Quit\n")
+      val input = StdIn.readInt()
+      input match {
+        case (input) if (input == 1) =>{
+          emojiCountAllStream(spark)
+        }
+        case (input) if (input == 2) =>{
+          println("What user do you want to search for?")
+          val user = StdIn.readLine()
+          emojiCountOneStream(spark, user)
+        }
+        case (input) if (input == 3) =>{
+          println("Exiting. . .\n")
+          System.exit(0)
+        }
+        case _ => {
+          println("Invalid. Here are your options:")
+        }
+      }
+    }
   }
 
-  def parseTweetStream(spark: SparkSession): Unit = {
+  /**
+    * Gets emoji usage for ALL users in the tweet stream
+    *
+    * @param spark
+    */
+  def emojiCountAllStream(spark: SparkSession): Unit = {
+    import spark.implicits._
+
+    val bearerToken = System.getenv("TWITTER_BEARER_TOKEN")
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+    Future {
+      downloadTweetStream(bearerToken, queryString = "?tweet.fields=entities&expansions=entities.mentions.username")
+    }
+
+    // Routine to wait for a file to appear in the twitterstream folder.
+    // Note: Ends the program if no file appears after 30 seconds.
+    var start = System.currentTimeMillis()
+    var filesFoundInDir = false
+    while (!filesFoundInDir && (System.currentTimeMillis() - start) < 30000) {
+      filesFoundInDir =
+        Files.list(Paths.get("twitterstream")).findFirst().isPresent()
+        Thread.sleep(500)
+    }
+    if (!filesFoundInDir) {
+      println(
+        "Error: Unable to populate tweetstream after 30 seconds. Exiting. . ."
+      )
+      System.exit(1)
+    }
+
+    val staticDf = spark.read.json("twitterstream")
+    val streamDf =
+      spark.readStream.schema(staticDf.schema).json("twitterstream")
+
+    val emoji = "[(\uD83D\uDE00-\uD83D\uDE4F)|(\uD83C\uDF00-\uD83D\uDDFF)|(\uD83E\uDD00-\uD83E\uDDFF)]"
+    val notEmoji = "[^(\uD83D\uDE00-\uD83D\uDE4F)|(\uD83C\uDF00-\uD83D\uDDFF)|(\uD83E\uDD00-\uD83E\uDDFF)]"
+    val regexSpace = "(\\B\uD83D.{1})|(\\B\uD83C.{1})|(\\B\uD83E.{1})"
+
+    streamDf
+      .select($"data.text", $"data.entities.mentions.username")
+      .filter($"includes".isNotNull)
+      .filter($"text" rlike s"${emoji}")
+      .select(regexp_replace($"text", s"${notEmoji}", "").as("Removed Words"), $"username")
+      .select(regexp_replace($"Removed Words", s"${regexSpace}", " $1").as("Added Space"), $"username")
+      .select(split($"Added Space", " ").as("Split"), $"username")
+      .select($"Split", explode($"username").as("Username"))
+      .select(explode($"Split").as("Emoji"), $"Username")
+      .filter($"Emoji" rlike s"${emoji}")
+      .filter(!$"Emoji".contains("(") && !$"Emoji".contains(")") && !$"Emoji".contains("|"))
+      .groupBy($"Username", $"Emoji")
+      .count()
+      .sort($"Username", $"Emoji")
+      .orderBy(desc("count"), $"Username")
+      .writeStream
+      .outputMode("complete")
+      .format("console")
+      .option("truncate", false)
+      .start()
+      .awaitTermination()
+  }
+
+  /**
+    * Gets emoji usage for ONE user from the tweet stream
+    *
+    * @param spark
+    * @param user
+    */
+  def emojiCountOneStream(spark: SparkSession, user: String): Unit = {
     import spark.implicits._
 
     val bearerToken = System.getenv("TWITTER_BEARER_TOKEN")
@@ -77,13 +171,14 @@ object Runner {
       .select(regexp_replace($"text2", s"${regexSpace}", " $1").as("split2"), $"username")
       .select(split($"split2", " ").as("split3"), $"username")
       .select($"split3", explode($"username").as("username"))
+      .filter($"username" rlike s"$user")
       .select(explode($"split3").as("exploded"), $"username")
       .filter($"exploded" rlike s"${emoji}")
       .filter(!$"exploded".contains("(") && !$"exploded".contains(")") && !$"exploded".contains("|"))
       .groupBy($"username", $"exploded")
       .count()
       .sort($"username", $"exploded")
-      .orderBy(desc("count"), $"username")
+      .orderBy($"username", desc("count"), $"exploded")
       .writeStream
       .outputMode("complete")
       .format("console")
